df5057140a764328854893432f389aef60fda437
LuciusBalian <lbalian@uwo.ca>
2026-02-13 23:27:20 -0500

    TextBubble Working

---
+++ Assets/Scripts/NPC/NPCLogic.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class NPCLogic : MonoBehaviour, IInteractable
+{
+    [SerializeField] private GameObject speechBubble;
+
+    public void Interact()
+    {
+        // Safety check
+        if (speechBubble == null)
+        {
+            return;
+        }
+
+        bool isCurrentlyActive = speechBubble.activeSelf;
+        speechBubble.SetActive(!isCurrentlyActive);
+
+        Debug.Log("NPC: Interaction Toggled");
+    }
+}
